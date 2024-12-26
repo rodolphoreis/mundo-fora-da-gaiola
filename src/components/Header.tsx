3825d8dface70d1d9436a829b855d1e0e102f69d
Rodolpho Reis <rodolphoreis@live.com>
2024-12-26 09:41:56 +0000

    style: remove ShimmerButton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,6 @@ import { LogIn } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Navbar } from "./Navbar";
-import ShimmerButton from "./ui/shimmer-button";
 
 export function Header() {
   return (
@@ -17,13 +16,14 @@ export function Header() {
         <Navbar />
 
         <div className="flex items-center gap-2 md:gap-5">
-          <Link href="/login" className="px-4 py-2 text-white  ">
-            <ShimmerButton className="shadow-2xl" background="#485342">
-              <span className="hidden sm:flex items-center gap-2">
-                Login <LogIn size={16} />
-              </span>
-              <LogIn className="block  sm:hidden " size={16} />
-            </ShimmerButton>
+          <Link
+            href="/login"
+            className="px-4 py-2 text-white underline underline-offset-4 "
+          >
+            <span className="hidden sm:flex items-center gap-2 font-medium">
+              LOGIN <LogIn size={16} />
+            </span>
+            <LogIn className="block  sm:hidden " size={16} />
           </Link>
         </div>
       </div>
