bce5e82b9c223067fae21bc078b124549e319720
Rodolpho Reis <rodolphoreis@live.com>
2024-12-26 11:11:24 +0000

    style: add align and justify center

--- src/components/VideoComponent.tsx
+++ src/components/VideoComponent.tsx
@@ -1,6 +1,12 @@
 export default async function VideoComponent() {
   const src = "https://www.youtube.com/embed/ecFoNPF5gok?start=1";
   return (
-    <iframe src={src} allowFullScreen className="w-full h-[315px] rounded-sm" />
+    <div className="flex w-full sm:h-[315px] align-center justify-center">
+      <iframe
+        src={src}
+        allowFullScreen
+        className="sm:w-full sm:h-[315px] rounded-sm "
+      />
+    </div>
   );
 }
