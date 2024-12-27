7a1ceaf2d899e9babd2a2c8ae79a39111e984a94
Rodolpho Reis <rodolphoreis@live.com>
2024-12-27 16:13:11 +0000

    feat: create new component

---
+++ src/components/Doubts.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import Transition from "./Transition/Transition";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+const Doubts = () => {
+  return (
+    <div className="w-full bg-customGreen flex flex-col justify-center items-center py-14">
+      <Transition className="flex flex-col items-center justify-center max-w-[1200px] gap-4 sm:gap-8 text-slate-100">
+        <h1 className="text-2xl sm:text-5xl font-bold sm:font-bold">
+          Ficou com dúvidas?
+        </h1>
+        <div className="flex flex-col items-center justify-center gap-1 text-lg sm:text-xl">
+          <p>Entre em contato por uma de nossas redes sociais!</p>
+          <p>Será um prazer te ajudar.</p>
+        </div>
+        <div className="flex gap-28">
+          <Link
+            href="https://www.facebook.com/blogpensandoforadagaiola/"
+            target="_blank"
+          >
+            <FaFacebook className="text-2xl sm:text-5xl text-slate-100" />
+          </Link>
+          <Link href="https://www.instagram.com/raphamundii/" target="_blank">
+            <FaInstagram className="text-2xl sm:text-5xl text-slate-100" />
+          </Link>
+          <Link
+            href="https://api.whatsapp.com/send/?phone=%2B5522999396776&text&type=phone_number&app_absent=0"
+            target="_blank"
+          >
+            <FaWhatsapp className="text-2xl sm:text-5xl text-slate-100" />
+          </Link>
+        </div>
+      </Transition>
+    </div>
+  );
+};
+
+export default Doubts;
