98ea495847c8ee3f913869af5e2ad07a02273a00
Rodolpho Reis <rodolphoreis@live.com>
2024-12-27 15:42:27 +0000

    style: change styles and add new img

--- src/components/AnnualSubscription.tsx
+++ src/components/AnnualSubscription.tsx
@@ -6,12 +6,24 @@ const AnnualSubscription = () => {
   return (
     <div className="w-full bg-customGreenSecondary flex flex-col sm:flex-row items-center justify-center">
       <Transition className=" py-14  md:px-16 flex flex-col sm:grid sm:grid-cols-[2fr,2fr] max-w-[1200px] mx-auto gap-5 ">
-        <div className="text-customGreen flex flex-col gap-4 p-2 sm:p-10 text-base sm:leading-7">
-          <h1 className="text-3xl md:text-4xl text-center leading-4">
-            O que você vai receber na
-            <br />
-            <span className="font-bold leading-4">Assinatura Anual:</span>
-          </h1>
+        <div className="text-customGreen flex flex-col gap-8 p-2 sm:p-10 text-base sm:leading-7">
+          <div className="flex flex-col gap-3">
+            <h1 className="text-3xl md:text-4xl text-center leading-4">
+              O que você vai receber na
+            </h1>
+            <h1 className="text-3xl md:text-4xl font-bold text-center leading-4">
+              Assinatura Anual:
+            </h1>
+          </div>
+          <div className="flex sm:hidden w-full justify-center">
+            <Image
+              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1006,h=474,fit=crop,trim=117.01492537313433;0;117.01492537313433;0/dOqNEMoz5zhyVXp6/cover-1-m5KlVvGNv5UEM6a1.png"
+              alt="imagem o mapa das milhas"
+              width={220}
+              height={50}
+              className="w-full rounded-md md:rounded-sm px-4 "
+            />
+          </div>
           <div className=" flex flex-col gap-1 sm:text-lg px-4">
             <p className=" flex gap-1 ">
               {" "}
@@ -59,7 +71,7 @@ const AnnualSubscription = () => {
             </p>
           </div>
         </div>
-        <div className="flex w-full justify-center">
+        <div className="hidden sm:flex w-full justify-center">
           <Image
             src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1006,h=474,fit=crop,trim=117.01492537313433;0;117.01492537313433;0/dOqNEMoz5zhyVXp6/cover-1-m5KlVvGNv5UEM6a1.png"
             alt="imagem o mapa das milhas"
