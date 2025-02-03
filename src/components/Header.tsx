fc5dbb719ec36ece74d0f32f0bdc56256f3335c6
Rodolpho Reis <rodolphoreis@live.com>
2025-02-03 16:51:06 +0000

    feat: add signedOut in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,29 +4,37 @@ import { LogIn } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Navbar } from "./Navbar";
+import { ClerkProvider, SignedOut, SignInButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 export function Header() {
   return (
-    <header className="w-full ">
-      <div className="max-w-[1200px] flex items-center justify-between pt-5 md:pt-10 md:px-0 mx-auto">
-        <Link href="/">
-          <Image src="/logo.png" alt="logo" width={330} height={200} />
-        </Link>
-
-        <Navbar />
-
-        <div className="flex items-center gap-2 md:gap-5">
-          <Link
-            href="/login"
-            className="px-4 py-2 text-white underline underline-offset-4 "
-          >
-            <span className="hidden sm:flex items-center gap-2 font-medium">
-              LOGIN <LogIn size={16} />
-            </span>
-            <LogIn className="block  sm:hidden " size={16} />
+    <ClerkProvider>
+      <header className="w-full ">
+        <div className="max-w-[1200px] flex items-center justify-between pt-5 md:pt-10 md:px-0 mx-auto">
+          <Link href="/">
+            <Image src="/logo.png" alt="logo" width={330} height={200} />
           </Link>
+
+          <Navbar />
+
+          <div className="flex  items-center gap-2 md:gap-5 mr-3">
+            <SignedOut>
+              <Button
+                variant={"link"}
+                className="px-4 py-2 text-white underline underline-offset-4 "
+              >
+                <span className="hidden sm:flex items-center gap-2 font-medium">
+                  <SignInButton />
+                  <LogIn size={16} />
+                </span>
+              </Button>
+            </SignedOut>
+
+            <LogIn className="block  sm:hidden " color="white" size={16} />
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </ClerkProvider>
   );
 }
